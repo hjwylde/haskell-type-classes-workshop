f3a95c650827cfc330df0b12584434771dde9b70
Henry J. Wylde <public@hjwylde.com>
2016-04-12 21:16:06 +1200

    Added a Parser newtype and helper functions

---
+++ src/Parser.hs
@@ -0,0 +1,72 @@
+module Parser where
+
+import Data.Char
+import Data.List
+
+import MyEither
+
+-- A parser takes some input and returns either an error (MyLeft), or the result with any leftovers
+-- from parsing (MyRight).
+newtype Parser a = Parser { runParser :: String -> MyEither String (a, String) }
+
+instance Functor Parser where
+    fmap _ _ = _
+
+instance Applicative Parser where
+    pure _ = _
+
+    _ <*> _ = _
+
+instance Monad Parser where
+    _ >>= _ = _
+
+-- Runs the given parser and expects it to consume the input fully.
+parse :: Parser a -> String -> MyEither String a
+parse parser input = do
+    (result, leftovers) <- runParser parser input
+
+    if null leftovers
+        then return result
+        else MyLeft $ "unexpected leftovers " ++ leftovers
+
+-- Utility for building up a parser.
+-- This will ensure that the input is non-empty and will split out the head and tail from the input
+-- before passing it to the given parsing function.
+parser :: ((Char, String) -> MyEither String (a, String)) -> Parser a
+parser f = Parser $ \input -> case uncons input of
+    Nothing             -> MyLeft "unexpected end of input"
+    Just (head, tail)   -> f (head, tail)
+
+-- Parses an identifier.
+-- An identifier starts with an alpha character and is followed by 0 or more alpha numeric
+-- characters.
+identifier :: Parser String
+identifier = parser $ \(head, tail) -> if isAlpha head
+    then return $ first (head:) (span isAlphaNum tail)
+    else MyLeft $ "expecting identifier, but found " ++ [head]
+
+-- Parses the given keyword.
+keyword :: String -> Parser ()
+keyword keyword = Parser $ \input -> do
+    (identifier', leftovers) <- runParser identifier input
+
+    if identifier' == keyword
+        then return $ ((), leftovers)
+        else MyLeft $ "expected keyword " ++ keyword ++ ", but found " ++ identifier'
+
+-- Parses the given symbol.
+symbol :: Char -> Parser ()
+symbol c = parser $ \(head, tail) -> if head == c
+    then return ((), tail)
+    else MyLeft $ "expecting " ++ [c] ++ ", but found " ++ [head]
+
+-- Parses an integer.
+-- An integer is 1 or more digits.
+int :: Parser Int
+int = parser $ \(head, tail) -> if isDigit head
+    then return $ first (read . (head:)) (span isDigit tail)
+    else MyLeft $ "expecting a digit, but found " ++ [head]
+
+-- Applies a function to the first element in a tuple.
+first :: (a -> c) -> (a, b) -> (c, b)
+first f (a, b) = (f a, b)
